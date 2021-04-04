fd0481854fe52512d2ce91099e150a6046ee9055
Samarth Lamba <22lambas@gmail.com>
2021-04-04 16:11:37 -0400

    added python again

---
+++ CSVtoMem.py
@@ -0,0 +1,13 @@
+import csv
+path = r"C:\Users\samar\Desktop\Semester 6\ECE350Labs\ECE350_Final_Project\assets\assetsCSV\\";
+savePath = r"""C:\Users\samar\Desktop\Semester 6\ECE350Labs\ECE350_Final_Project\assets\assetsMemFiles""";
+def CSVtoMem(imageName):
+    with open(path + imageName + "colors.csv", "r") as csvFile:
+        with open(savePath +r"\\"+ imageName+ "colors.mem", "w") as memFile:
+                for row in csv.reader(csvFile):
+                    memFile.write(" ".join(row) + "\n")
+
+    with open(path + imageName+ "image.csv", "r") as csvFile:
+        with open(savePath + imageName + "image.mem", "w") as memFile:
+            for row in csv.reader(csvFile):
+                memFile.write(" ".join(row) + "\n")
