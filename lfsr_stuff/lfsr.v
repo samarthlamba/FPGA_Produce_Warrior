0d34505e6ab8cc7358abf870a45ecc5e86bea52b
Samarth Lamba <22lambas@gmail.com>
2021-04-10 22:56:18 -0400

    #6 ouputting 1 bits randomly

--- lfsr_stuff/lfsr.v
+++ lfsr_stuff/lfsr.v
@@ -13,12 +13,28 @@ module lfsr(out, clk, clr, seed, select);
     wire num_two;
     wire num_out;
     
-    dffe_ref ff[7:0] (ffout, ffin, clk, 1'b1, clr);
-    mux_2_1 mux[7:0] (ffin, select, seed, {ffout[6], ffout[5], ffout[4], ffout[3], ffout[2], ffout[1], ffout[0], num_out});
+    dffe_ref dffe(ffout[0], ffin[0], clk, 1'b1, clr);
+    dffe_ref dffe2(ffout[1], ffin[1], clk, 1'b1, clr);
+    dffe_ref dffe3(ffout[2], ffin[2], clk, 1'b1, clr);
+    dffe_ref dffe4(ffout[3], ffin[3], clk, 1'b1, clr);
+    dffe_ref dffe5(ffout[4], ffin[4], clk, 1'b1, clr);
+    dffe_ref dffe6(ffout[5], ffin[5], clk, 1'b1, clr);
+    dffe_ref dffe7(ffout[6], ffin[6], clk, 1'b1, clr);
+    dffe_ref dffe8(ffout[7], ffin[7], clk, 1'b1, clr);
 
-    xor n1(num_one, ffout[7], ffout[5]);
-    xor n2(num_two, ffout[4], num_one);
-    xor n3(num_out, ffout[3], num_two);
+    mux_2_1 mux1(ffin[0], select, seed[0], num_out);
+    mux_2_1 mux2(ffin[1], select, seed[1], ffout[0]);
+    mux_2_1 mux3(ffin[2], select, seed[2], ffout[1]);
+    mux_2_1 mux4(ffin[3], select, seed[3], ffout[2]);
+    mux_2_1 mux5(ffin[4], select, seed[4], ffout[3]);
+    mux_2_1 mux6(ffin[5], select, seed[5], ffout[4]);
+    mux_2_1 mux7(ffin[6], select, seed[6], ffout[5]);
+    mux_2_1 mux8(ffin[7], select, seed[7], num_out);
+
+
+    xor n2(num_one, ffout[5], ffout[7]);
+    xor n3(num_two,num_one, ffout[4]);
+    xor n4(num_out,num_two, ffout[3]);
 
     assign out = num_out;
 
--- lfsr_stuff/lfsr_tb.v
+++ lfsr_stuff/lfsr_tb.v
@@ -1,29 +1,47 @@
 `timescale 1 ns /100 ps
 module lfsr_tb;
     reg clk;
-    reg clr;
-    reg [7:0] seed;
-    reg select, reset;
-    wire [3:0] out;
+reg rst;
+reg [7:0] seed;
+reg load;
+wire [7:0] total;
+wire q;
+lfsr L(q, clk, rst,
+seed, load);
 
-    lfsr check(out, clk, reset);
-   initial
-begin
-    clk = 0;
-    reset = 1;
-    #15;
-    
-    reset = 0;
-    #200;
+    initial
+        begin
+            clk = 0;
+            load = 0;
+            seed = 7'b0011;
+            rst = 0;
+            #10 rst = 1;
+            #10 rst = 0;
+        end
+// drive clock
+always
+    begin
+        #50 clk = !clk;
+    end
+// program lfsr
+initial begin
+    #100 seed = 4'b0011;
+    load = 1;
+    #4500 load = 0;
+
 end
 
-    always 
-        #30 clk = ~clk;
-    
+    genvar c;
+    generate     
+    for (c = 0; c < 8; c = c + 1) begin: test
+      
+    assign total[c] = q;
+    end
+    endgenerate
 
-    always @(out) begin
+    always @(q, load, seed) begin
     #1;
-    $display("Output:%d",out);
+    $display("Output:%b, %b, %d",q, load, total);
 
 end
 endmodule
