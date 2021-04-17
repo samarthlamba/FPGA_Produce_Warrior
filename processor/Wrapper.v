b0011080e77b70ed30e91e8ae559ef4c6762ec17
Reya Magan <reya.magan@duke.edu>
2021-04-16 23:37:18 -0400

    wrapper update

--- processor/Wrapper.v
+++ processor/Wrapper.v
@@ -24,7 +24,6 @@
  *
  **/
 
-<<<<<<< HEAD
 module Wrapper (clock, reset, sclk, mosi, miso, ss, up, down, left, right, restx, resty, hSync, VSync, VGA_R, VGA_B, VGA_G, up_fpga, down_fpga, right_fpga, left_fpga, ps2_clk, ps2_data);
 	input clock, reset, miso;
 	output sclk, mosi, ss;
@@ -35,19 +34,54 @@ module Wrapper (clock, reset, sclk, mosi, miso, ss, up, down, left, right, restx
 	inout ps2_clk;
 	inout ps2_data;
 	input up_fpga, down_fpga, right_fpga, left_fpga;
-=======
-module Wrapper (clock, reset);
-	input clock, reset;
->>>>>>> 27aaa832d232e199904e35b3ad0cbceb9ffe9075
 
+    reg up1, down1, left1, right1, restx1, resty1;
+
+	wire[8:0] accel_x, accel_y;
+	wire[11:0] accel_z;
 	wire rwe, mwe;
 	wire[4:0] rd, rs1, rs2;
 	wire[31:0] instAddr, instData, 
 		rData, regA, regB,
 		memAddr, memDataIn, memDataOut;
+	
+	
+	always @(posedge clock) begin
+	    if(accel_x == 385)
+	       restx1 = 1'b1;
+	    else
+	       restx1 = 1'b0;  
+		if(accel_x < 385)
+		   down1 = 1'b1;
+		else
+		   down1 = 1'b0;
+		if(accel_x > 385)
+		   up1 = 1'b1;
+		else
+		   up1 = 1'b0;
+		if(accel_y == 80)
+		   resty1 = 1'b1;
+		else
+		   resty1 = 1'b0;
+		if(accel_y < 80)
+		   left1 = 1'b1;
+		else
+		   left1 = 1'b0;
+		if(accel_y > 80)
+		   right1 = 1'b1;
+		else
+		   right1 = 1'b0;
+	end
+	
+	assign up = up1;
+	assign down = down1;
+	assign right = right1;
+	assign left = left1;
+	assign restx = restx1;
+	assign resty = resty1;
 
+	AccelerometerCtl accelerometer(.SYSCLK(clock), .RESET(reset), .SCLK(sclk), .MOSI(mosi), .MISO(miso), .SS(ss), .ACCEL_X_OUT(accel_x), .ACCEL_Y_OUT(accel_y), .ACCEL_MAG_OUT(accel_z));
 
-<<<<<<< HEAD
 	VGAController vga(     
 	 clock, 			
 	 reset, 	
@@ -64,45 +98,42 @@ module Wrapper (clock, reset);
 	 ps2_data,
 	 accel_x,
 	 accel_y);
-=======
->>>>>>> 27aaa832d232e199904e35b3ad0cbceb9ffe9075
-	// ADD YOUR MEMORY FILE HERE
-	localparam INSTR_FILE = "";
+//	// ADD YOUR MEMORY FILE HERE
+//	localparam INSTR_FILE = "";
 	
-	//Accel
-	// Main Processing Unit
-	processor CPU(.clock(clock), .reset(reset), 
+//	// Main Processing Unit
+//	processor CPU(.clock(clock), .reset(reset), 
 								
-		// ROM
-		.address_imem(instAddr), .q_imem(instData),
+//		// ROM
+//		.address_imem(instAddr), .q_imem(instData),
 									
-		// Regfile
-		.ctrl_writeEnable(rwe),     .ctrl_writeReg(rd),
-		.ctrl_readRegA(rs1),     .ctrl_readRegB(rs2), 
-		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB),
+//		// Regfile
+//		.ctrl_writeEnable(rwe),     .ctrl_writeReg(rd),
+//		.ctrl_readRegA(rs1),     .ctrl_readRegB(rs2), 
+//		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB),
 									
-		// RAM
-		.wren(mwe), .address_dmem(memAddr), 
-		.data(memDataIn), .q_dmem(memDataOut)); 
+//		// RAM
+//		.wren(mwe), .address_dmem(memAddr), 
+//		.data(memDataIn), .q_dmem(memDataOut)); 
 	
-	// Instruction Memory (ROM)
-	ROM #(.MEMFILE({INSTR_FILE, ".mem"}))
-	InstMem(.clk(clock), 
-		.addr(instAddr[11:0]), 
-		.dataOut(instData));
+//	// Instruction Memory (ROM)
+//	ROM #(.MEMFILE({INSTR_FILE, ".mem"}))
+//	InstMem(.clk(clock), 
+//		.addr(instAddr[11:0]), 
+//		.dataOut(instData));
 	
-	// Register File
-	regfile RegisterFile(.clock(clock), 
-		.ctrl_writeEnable(rwe), .ctrl_reset(reset), 
-		.ctrl_writeReg(rd),
-		.ctrl_readRegA(rs1), .ctrl_readRegB(rs2), 
-		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB));
+//	// Register File
+//	regfile RegisterFile(.clock(clock), 
+//		.ctrl_writeEnable(rwe), .ctrl_reset(reset), 
+//		.ctrl_writeReg(rd),
+//		.ctrl_readRegA(rs1), .ctrl_readRegB(rs2), 
+//		.data_writeReg(rData), .data_readRegA(regA), .data_readRegB(regB));
 						
-	// Processor Memory (RAM)
-	RAM ProcMem(.clk(clock), 
-		.wEn(mwe), 
-		.addr(memAddr[11:0]), 
-		.dataIn(memDataIn), 
-		.dataOut(memDataOut));
+//	// Processor Memory (RAM)
+//	RAM ProcMem(.clk(clock), 
+//		.wEn(mwe), 
+//		.addr(memAddr[11:0]), 
+//		.dataIn(memDataIn), 
+//		.dataOut(memDataOut));
 
 endmodule
