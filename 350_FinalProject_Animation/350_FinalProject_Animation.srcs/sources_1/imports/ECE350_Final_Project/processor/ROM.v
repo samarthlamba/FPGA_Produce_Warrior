1b95e2ec5291e5e0c2f7dc332fddf3a66ac8d28b
Samarth Lamba <22lambas@gmail.com>
2021-04-18 03:12:25 -0400

    fixed mem file issue

--- 350_FinalProject_Animation/350_FinalProject_Animation.srcs/sources_1/imports/ECE350_Final_Project/processor/ROM.v
+++ 350_FinalProject_Animation/350_FinalProject_Animation.srcs/sources_1/imports/ECE350_Final_Project/processor/ROM.v
@@ -8,7 +8,7 @@ module ROM #(parameter DATA_WIDTH = 32, ADDRESS_WIDTH = 12, DEPTH = 4096, MEMFIL
     
     initial begin
         if(MEMFILE > 0) begin
-            $readmemb(MEMFILE, MemoryArray);
+            $readmemh(MEMFILE, MemoryArray);
         end
     end
     
