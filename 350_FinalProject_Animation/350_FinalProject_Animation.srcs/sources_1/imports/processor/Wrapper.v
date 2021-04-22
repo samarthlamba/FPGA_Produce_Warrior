d441fa8942d6c4cc159d91c895fb52ceba7fb885
Samarth Lamba <22lambas@gmail.com>
2021-04-21 20:44:59 -0400

    not shaking

--- 350_FinalProject_Animation/350_FinalProject_Animation.srcs/sources_1/imports/processor/Wrapper.v
+++ 350_FinalProject_Animation/350_FinalProject_Animation.srcs/sources_1/imports/processor/Wrapper.v
@@ -175,7 +175,7 @@ module Wrapper (clock, reset, sclk, mosi, miso, ss, up, down, left, right, restx
 	.reg_out2(reg_2_x), .reg_out3(reg_3_x), .reg_out4(reg_4_x), .reg_out5(reg_5_x), .reg_out6(reg_6_x), .reg_out7(reg_7_x),
 	.reg_out8(reg_8_x), .reg_out9(reg_9_y), .reg_out10(reg_10_y), .reg_out11(reg_11_y), .reg_out12(reg_12_y), .reg_out13(reg_13_y),
 	.reg_out14(reg_14_y), .reg_out15(reg_15_y), .reg_out16(reg_16_y), .reg_out29(reg_29_rand));
-     assign LED_out = instData == 32'd0;   
+     assign LED_out = reg_16_y == 32'd0;   
      assign LED_out2 = isStalling;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
 // Processor Memory (RAM)
 	RAMproc ProcMem(.clk(clk50), 
